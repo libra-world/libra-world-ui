45076afa8092c9aa902cabd69932450b5d1ea564
zhongweibin <sunshy@binance.com>
2019-10-24 00:01:02 +0800

    feat: move to store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import get from 'lodash/get';
 import BgCard from '@src/pages/Home/BgCard';
 import { Box } from '@src/components/uikit';
 import TickerSortRow from '@src/pages/Home/TickerSortRow';
 import TickersList from '@src/pages/Home/TickersList';
 // import Summary from '@src/pages/Home/Summary';
-import { getTransactionTXList } from '@src/util/request';
+import store from '@src/store';
 
 const MarketBox = styled(Box)`
   box-shadow: ${({ theme }) => theme.colors.boxShadow};
@@ -17,38 +18,17 @@ const TickersListStyled = styled(Box)`
 const ROW_HEIGHT = 70;
 
 function HomePage() {
+  const data = useSelector(state => state.home.data);
   const [sortObj, setSortObj] = useState({
     sortBy: 'qv',
     sortDirection: 'DESC',
   });
-  const [data, setData] = useState({ total: 0, list: [] });
   const onRowClick = () => {};
-  const onLoadMore = React.useCallback(
-    async ({ page, pageSize }) => {
-      const resp = await getTransactionTXList({ currentPage: page, sizePage: pageSize });
-      if (get(resp, ['data', 'success'])) {
-        setData(({ list }) => ({
-          total: get(resp, ['data', 'data', 'totalCount']),
-          list: list
-            .slice(0, page * pageSize - 1)
-            .concat(get(resp, ['data', 'data', 'data']))
-            .concat(data.list.slice((page + 1) * pageSize - 1)),
-        }));
-      }
-    },
-    [setData]
-  );
-  console.log('data', data);
-  React.useEffect(() => {
-    getTransactionTXList().then(resp => {
-      if (get(resp, ['data', 'success'])) {
-        setData({
-          total: get(resp, ['data', 'data', 'totalCount']),
-          list: get(resp, ['data', 'data', 'data']),
-        });
-      }
-    });
+  const onLoadMore = React.useCallback(async ({ page, pageSize }) => {
+    await store.dispatch.home.requestTransactionTXList({ currentPage: page, sizePage: pageSize });
   }, []);
+  console.log('data', data);
+
   return (
     <>
       <BgCard />
--- src/pages/Home/model.js
+++ src/pages/Home/model.js
@@ -1,12 +1,64 @@
+import { getTransactionTXList } from '@src/util/request';
+import get from 'lodash/get';
+
 export default {
   state: {
     test: "it's a test value",
     searchString: '',
     data: {
-      total: 0,
+      total: 10,
       list: [],
     },
   },
-  reducers: {},
-  effects: {},
+  reducers: {
+    setData(prevState, payload) {
+      console.log('prevState', prevState);
+      console.log('payload', payload);
+      return {
+        ...prevState,
+        data: payload,
+      };
+    },
+    setFields(prevState, payload) {
+      return {
+        ...prevState,
+        ...payload,
+      };
+    },
+  },
+  effects: {
+    async requestTransactionTXList(payload = { currentPage: 1, sizePage: 10 }, rootState) {
+      const resp = await getTransactionTXList({
+        ...payload,
+        search: rootState.home.searchString,
+      });
+      if (get(resp, ['data', 'success'])) {
+        this.setData({
+          total: get(resp, ['data', 'data', 'totalCount']),
+          list: rootState.home.data.list
+            .slice(0, payload.currentPage * payload.sizePage - 1)
+            .concat(get(resp, ['data', 'data', 'data']))
+            .concat(
+              rootState.home.data.list.slice((payload.currentPage + 1) * payload.sizePage - 1)
+            ),
+        });
+      }
+    },
+    async setSearch(payload, rootState) {
+      this.setFields({ searchString: payload });
+      const resp = await getTransactionTXList({
+        currentPage: 1,
+        sizePage: 10,
+        search: payload,
+      });
+      if (get(resp, ['data', 'success'])) {
+        this.setFields({
+          data: {
+            total: get(resp, ['data', 'data', 'totalCount']),
+            list: get(resp, ['data', 'data', 'data']),
+          },
+        });
+      }
+    },
+  },
 };
